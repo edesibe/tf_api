7bcf83921a0cc7b3d3edcb9c0c1820b859b170a0
Ivan Milivojevic <ivan.milivojevic@endava.com>
2019-02-02 12:11:34 +0100

    update

--- main.tf
+++ main.tf
@@ -31,6 +31,14 @@ resource "aws_elb" "api" {
     lb_port           = 80
     lb_protocol       = "http"
   }
+  
+  health_check {
+    healthy_threshold   = 3
+    unhealthy_threshold = 2
+    timeout             = 5
+    target              = "TCP:80"
+    interval            = 30
+  }
 
   instances = ["${aws_instance.api.*.id}"]
 }
